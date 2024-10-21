2e67c9c2643f38772e72e6808ab0e44d3c1b2ca7
Walter <wjesus88@gmail.com>
2024-10-21 13:02:09 -0500

    add jwt

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Usa una imagen base de Python
-FROM python:3.9
+FROM python:3.11
 
 # Establece el directorio de trabajo
 WORKDIR /app
--- myapp/myapp/urls.py
+++ myapp/myapp/urls.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from rest_framework import routers, viewsets, serializers
 from django.contrib import admin
 
+from rest_framework.authtoken import views
+
 #from . import views
 
 
@@ -39,4 +41,15 @@ urlpatterns = [
 # Agregamos el inicio de sesión y cierre de sesión de la API
 urlpatterns += [
     path('api-auth/', include('rest_framework.urls')),
+    
+]
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
