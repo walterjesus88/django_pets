b6f042f9f6bd935aca84b6a56f7d263e75f1cc14
Walter <wjesus88@gmail.com>
2024-10-28 19:05:36 -0500

    add

--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -16,6 +16,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
+    'corsheaders',
     'snippets',
 ]
 
@@ -41,6 +42,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 DATABASES = {
